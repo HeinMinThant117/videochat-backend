e75e566a20c3d902c16e87f95e89dd734a4dc4e2
Hein <heinminthant1999@gmail.com>
2024-04-29 20:51:42 +0700

    chore: add sefault fasity route

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,35 @@
-console.log("Hello World!");
+import Fasitfy, { FastifyInstance, RouteShorthandOptions } from "fastify";
+import { Server, IncomingMessage, ServerResponse } from "http";
+
+const server: FastifyInstance = Fasitfy({});
+
+const opts: RouteShorthandOptions = {
+  schema: {
+    response: {
+      200: {
+        type: "object",
+        properties: {
+          pong: {
+            type: "string",
+          },
+        },
+      },
+    },
+  },
+};
+
+server.get("/ping", opts, async (request, reply) => {
+  return { pong: "pong" };
+});
+
+const start = async () => {
+  try {
+    await server.listen({ port: 3000 });
+    console.log(`Server is listening on PORT 3000`);
+  } catch (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
