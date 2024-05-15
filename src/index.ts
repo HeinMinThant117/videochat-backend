881735f0d7e001be27755617853ae478c6dd7a83
Hein <heinminthant1999@gmail.com>
2024-05-15 10:44:16 +0700

    refactor: move fastify app to separate component

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,5 @@
-import Fastify from "fastify";
-import ourDbConnector from "./our-db-connector";
-import authRoutes from "./routes/auth";
-import fastifyStatic from "@fastify/static";
 import "dotenv/config";
-import path = require("path");
-import cors from "@fastify/cors";
-
-const fastify = Fastify({
-  logger: true,
-});
-
-fastify.register(fastifyStatic, {
-  root: path.join(__dirname, "public"),
-  prefix: "/public/",
-});
-
-fastify.register(ourDbConnector);
-fastify.register(authRoutes);
-fastify.register(cors, {});
-
-fastify.get("/", (_request, reply) => {
-  return reply.sendFile("index.html");
-});
+import fastify from "./app";
 
 async function start() {
   try {
